72bd0f8efc80eac8200942c72bf5f886e53f47b9
phil65 <philipptemminghoff@gmx.de>
2016-03-17 23:40:59 +0100

    cleanup

--- resources/lib/appfeed.py
+++ resources/lib/appfeed.py
@@ -4,18 +4,13 @@ from BeautifulSoup import BeautifulSoup
 import common
 import listtv
 import listmovie
-
+import xbmcgui
+import json
 pluginhandle = common.pluginhandle
 xbmc = common.xbmc
-xbmcplugin = common.xbmcplugin
 urllib = common.urllib
-sys = common.sys
-xbmcgui = common.xbmcgui
-re = common.re
-json = common.json
-os = common.os
 
-#Modes
+# Modes
 #===============================================================================
 # 'catalog/GetCategoryList'
 # 'catalog/Browse'
@@ -45,55 +40,54 @@ os = common.os
 
 deviceID = common.gen_id()
 
-#Android id: A2W5AJPLW5Q6YM, A1PY8QQU9P0WJV, A1MPSLFC7L5AFK // fmw:{AndroidSDK}-app:{AppVersion}
-#deviceTypeID = 'A13Q6A55DBZB7M' #WEB Type
-firmware = 'fmw:15-app:1.1.19' #Android
-#firmware = 'fmw:10-app:1.1.23'
-#deviceTypeID = 'A3VN4E5F7BBC7S' #Roku
-#firmware = 'fmw:045.01E01164A-app:4.7'
+firmware = 'fmw:15-app:1.1.19'  # Android
 deviceTypeID = 'A1MPSLFC7L5AFK'
 format = 'json'
 
-PARAMETERS = '?firmware='+firmware+'&deviceTypeID='+deviceTypeID+'&deviceID='+deviceID+'&format='+format
+PARAMETERS = '?firmware=' + firmware + '&deviceTypeID=' + deviceTypeID + '&deviceID=' + deviceID + '&format=' + format
 
-def BUILD_BASE_API(MODE,HOST=common.ATV_URL + '/cdp/'):
-    return HOST+MODE+PARAMETERS
 
-def getList(ContentType=False,start=0,isPrime=True,NumberOfResults=False,OrderBy='MostPopular',version=2,AsinList=False,catalog='Browse',asin=False):
-    BROWSE_PARAMS ='&StartIndex='+str(start)
-    if NumberOfResults: BROWSE_PARAMS +='&NumberOfResults='+str(NumberOfResults)
+def BUILD_BASE_API(MODE, HOST=common.ATV_URL + '/cdp/'):
+    return HOST + MODE + PARAMETERS
+
+
+def getList(ContentType=False, start=0, isPrime=True, NumberOfResults=False, OrderBy='MostPopular', version=2, AsinList=False, catalog='Browse', asin=False):
+    BROWSE_PARAMS = '&StartIndex=' + str(start)
+    if NumberOfResults:
+        BROWSE_PARAMS += '&NumberOfResults=' + str(NumberOfResults)
     if ContentType:
-        BROWSE_PARAMS +='&ContentType='+ContentType
-        BROWSE_PARAMS +='&OrderBy='+OrderBy
-    BROWSE_PARAMS +='&IncludeAll=T'
-    if isPrime: BROWSE_PARAMS += '&OfferGroups=B0043YVHMY'
+        BROWSE_PARAMS += '&ContentType=' + ContentType
+        BROWSE_PARAMS += '&OrderBy=' + OrderBy
+    BROWSE_PARAMS += '&IncludeAll=T'
+    if isPrime:
+        BROWSE_PARAMS += '&OfferGroups=B0043YVHMY'
     if ContentType == 'TVEpisode':
-        BROWSE_PARAMS +='&Detailed=T'
-        BROWSE_PARAMS +='&AID=T'
-        BROWSE_PARAMS +='&tag=1'
-        BROWSE_PARAMS +='&IncludeBlackList=T'
-    if AsinList: BROWSE_PARAMS +='&SeasonASIN='+AsinList
-    if asin: BROWSE_PARAMS +='&asin='+asin
-    BROWSE_PARAMS +='&version='+str(version)
-    #&HighDef=F # T or F ??
-    #&playbackInformationRequired=false
-    #&OrderBy=SalesRank
-    #&SuppressBlackedoutEST=T
-    #&HideNum=T
-    #&Detailed=T
-    #&AID=1
-    #&IncludeNonWeb=T
-    url = BUILD_BASE_API('catalog/%s' % catalog)+BROWSE_PARAMS
+        BROWSE_PARAMS += '&Detailed=T'
+        BROWSE_PARAMS += '&AID=T'
+        BROWSE_PARAMS += '&tag=1'
+        BROWSE_PARAMS += '&IncludeBlackList=T'
+    if AsinList:
+        BROWSE_PARAMS += '&SeasonASIN=' + AsinList
+    if asin:
+        BROWSE_PARAMS += '&asin=' + asin
+    BROWSE_PARAMS += '&version=' + str(version)
+    url = BUILD_BASE_API('catalog/%s' % catalog) + BROWSE_PARAMS
     return json.loads(common.getATVURL(url))
+
 
 def ASIN_LOOKUP(ASINLIST):
-    results = len(ASINLIST.split(','))-1
-    BROWSE_PARAMS = '&asinList='+ASINLIST+'&NumberOfResults='+str(results)+'&IncludeAll=T&playbackInformationRequired=true&version=2'
-    url = BUILD_BASE_API('catalog/GetASINDetails')+BROWSE_PARAMS
+    params = {"asinList": ASINLIST,
+              "NumberOfResults": len(ASINLIST.split(',')) - 1,
+              "IncludeAll": "T",
+              "playbackInformationRequired": "true",
+              "version": 2}
+    url = BUILD_BASE_API('catalog/GetASINDetails&') + urllib.urlencode(params)
     return json.loads(common.getATVURL(url))
 
+
 def URL_LOOKUP(url):
-    return json.loads(common.getATVURL(url+PARAMETERS.replace('?','&')))
+    return json.loads(common.getATVURL(url + PARAMETERS.replace('?', '&')))
+
 
 def SEARCH_DB(searchString=False):
     if not searchString:
@@ -101,8 +95,8 @@ def SEARCH_DB(searchString=False):
         keyboard.doModal()
         q = keyboard.getText()
         if (keyboard.isConfirmed()):
-            searchString=keyboard.getText()
-            if searchString <> '':
+            searchString = keyboard.getText()
+            if searchString != '':
                 common.addText('          ----=== ' + common.getString(30104) + ' ===----')
                 if not listmovie.LIST_MOVIES('movietitle', searchString, search=True):
                     common.addText(common.getString(30202))
@@ -111,26 +105,31 @@ def SEARCH_DB(searchString=False):
                     common.addText(common.getString(30202))
                 common.SetView('tvshows', 'showview')
 
+
 def ExportList():
     list_ = common.args.url
     ListCont(common.movielib % list_)
     ListCont(common.tvlib % list_)
 
+
 def getSimilarities():
     import tv
-    data = getList(NumberOfResults=250,catalog='GetSimilarities',asin=common.args.asin)
+    data = getList(NumberOfResults=250, catalog='GetSimilarities', asin=common.args.asin)
     for title in data['message']['body']['titles']:
         asin = title['titleId']
         if not listmovie.LIST_MOVIES('asin', asin, search=True):
             for seasondata in tv.lookupTVdb(asin, tbl='seasons', single=False):
-                if seasondata: listtv.ADD_SEASON_ITEM(seasondata, disptitle=True)
+                if seasondata:
+                    listtv.ADD_SEASON_ITEM(seasondata, disptitle=True)
     common.SetView('tvshows', 'showview')
+
 
 def ListMenu():
     list_ = common.args.url
     common.addDir(common.getString(30104), 'appfeed', 'ListCont', common.movielib % list_)
     common.addDir(common.getString(30107), 'appfeed', 'ListCont', common.tvlib % list_)
     common.xbmcplugin.endOfDirectory(common.pluginhandle)
+
 
 def ListCont(export=False):
     import tv
@@ -140,8 +139,10 @@ def ListCont(export=False):
     if export:
         url = export
         export = True
-    else: url = common.args.url
-    if 'movie' in url: mov = True
+    else:
+        url = common.args.url
+    if 'movie' in url:
+        mov = True
     if common.addon.getSetting('disptvshow') == 'true':
         showonly = True
         rvalue = 'seriesasin'
@@ -153,7 +154,8 @@ def ListCont(export=False):
     asinlist = []
     for value in asins:
         ret = 0
-        if mov: ret = listmovie.LIST_MOVIES('asin', value, search=True, cmmode=1, export=export)
+        if mov:
+            ret = listmovie.LIST_MOVIES('asin', value, search=True, cmmode=1, export=export)
         if ret == 0 and not mov:
             for seasondata in tv.lookupTVdb(value, tbl='seasons', rvalue=rvalue, single=False):
                 if seasondata:
@@ -161,16 +163,21 @@ def ListCont(export=False):
                         ret = 0
                         value = seasondata[0]
                         for asin in tv.lookupTVdb(value, tbl='shows', rvalue='asin').split(','):
-                            if asin in asinlist: ret = 1
+                            if asin in asinlist:
+                                ret = 1
                     else:
                         ret = 1
                         listtv.ADD_SEASON_ITEM(seasondata, disptitle=True, cmmode=1, export=export)
-        if ret == 0 and not mov: listtv.LIST_TVSHOWS('asin', value, search=True, cmmode=1, export=export)
+        if ret == 0 and not mov:
+            listtv.LIST_TVSHOWS('asin', value, search=True, cmmode=1, export=export)
         asinlist.append(value)
 
     if not export:
-        if mov: common.SetView('movies', 'movieview')
-        else: common.SetView('tvshows', 'showview')
+        if mov:
+            common.SetView('movies', 'movieview')
+        else:
+            common.SetView('tvshows', 'showview')
+
 
 def RefreshList():
     import tv
@@ -182,17 +189,24 @@ def RefreshList():
     pDialog.create(common.__plugin__, common.getString(30117))
 
     for asin in common.SCRAP_ASINS(common.movielib % list):
-        if not movies.lookupMoviedb(asin): mvlist.append(asin)
+        if not movies.lookupMoviedb(asin):
+            mvlist.append(asin)
 
     for asin in common.SCRAP_ASINS(common.tvlib % list):
-        if not tv.lookupTVdb(asin, tbl='seasons'): tvlist.append(asin)
+        if not tv.lookupTVdb(asin, tbl='seasons'):
+            tvlist.append(asin)
 
-    if mvlist: movies.updateLibrary(mvlist)
-    if tvlist: tv.addTVdb(False, tvlist)
+    if mvlist:
+        movies.updateLibrary(mvlist)
+    if tvlist:
+        tv.addTVdb(False, tvlist)
     pDialog.close()
 
-    if mvlist: movies.updateFanart()
-    if tvlist: tv.updateFanart()
+    if mvlist:
+        movies.updateFanart()
+    if tvlist:
+        tv.updateFanart()
+
 
 def getTVDBImages(title, imdb=None, id=None, seasons=False):
     posterurl = fanarturl = None
@@ -214,7 +228,8 @@ def getTVDBImages(title, imdb=None, id=None, seasons=False):
                     break
             if title == oldtitle:
                 break
-    if not id: return None, None, None
+    if not id:
+        return None, None, None
     if seasons:
         soup = BeautifulSoup(common.getURL('http://www.thetvdb.com/api/%s/series/%s/banners.xml' % (common.tvdb, id), silent=True))
         seasons = {}
@@ -222,11 +237,13 @@ def getTVDBImages(title, imdb=None, id=None, seasons=False):
             for datalang in soup.findAll('language'):
                 if datalang.string == lang:
                     data = datalang.parent
-                    if data.bannertype.string == 'fanart' and not fanarturl: fanarturl = TVDB_URL + data.bannerpath.string
-                    if data.bannertype.string == 'poster' and not posterurl: posterurl = TVDB_URL + data.bannerpath.string
+                    if data.bannertype.string == 'fanart' and not fanarturl:
+                        fanarturl = TVDB_URL + data.bannerpath.string
+                    if data.bannertype.string == 'poster' and not posterurl:
+                        posterurl = TVDB_URL + data.bannerpath.string
                     if data.bannertype.string == data.bannertype2.string == 'season':
                         snr = data.season.string
-                        if not seasons.has_key(snr):
+                        if snr not in seasons:
                             seasons[snr] = TVDB_URL + data.bannerpath.string
         return seasons, posterurl, fanarturl
     else:
@@ -235,10 +252,14 @@ def getTVDBImages(title, imdb=None, id=None, seasons=False):
             soup = BeautifulSoup(result)
             fanart = soup.find('fanart')
             poster = soup.find('poster')
-            if len(fanart) and not fanarturl: fanarturl = TVDB_URL + fanart.string
-            if len(poster) and not posterurl : posterurl = TVDB_URL + poster.string
-            if posterurl and fanarturl: return id, posterurl, fanarturl
+            if len(fanart) and not fanarturl:
+                fanarturl = TVDB_URL + fanart.string
+            if len(poster) and not posterurl:
+                posterurl = TVDB_URL + poster.string
+            if posterurl and fanarturl:
+                return id, posterurl, fanarturl
         return id, posterurl, fanarturl
+
 
 def getTMDBImages(title, imdb=None, content='movie', year=None):
     fanart = poster = id = None
@@ -248,7 +269,8 @@ def getTMDBImages(title, imdb=None, content='movie', year=None):
 
     while not id:
         str_year = ''
-        if year: str_year = '&year=' + str(year)
+        if year:
+            str_year = '&year=' + str(year)
         movie = urllib.quote_plus(title)
         result = common.getURL('http://api.themoviedb.org/3/search/%s?api_key=%s&language=de&query=%s%s' % (content, common.tmdb, movie, str_year), silent=True)
         if not result:
@@ -258,8 +280,10 @@ def getTMDBImages(title, imdb=None, content='movie', year=None):
         data = json.loads(result)
         if data['total_results'] > 0:
             result = data['results'][0]
-            if result['backdrop_path']: fanart = TMDB_URL + result['backdrop_path']
-            if result['poster_path']: poster = TMDB_URL + result['poster_path']
+            if result['backdrop_path']:
+                fanart = TMDB_URL + result['backdrop_path']
+            if result['poster_path']:
+                poster = TMDB_URL + result['poster_path']
             id = result['id']
         elif year:
             year = 0
@@ -276,15 +300,17 @@ def getTMDBImages(title, imdb=None, content='movie', year=None):
         fanart = common.na
     return fanart
 
+
 def updateAll():
-    if common.updateRunning(): return
+    if common.updateRunning():
+        return
     import movies
     import tv
     from datetime import datetime
     common.addon.setSetting('update_running', datetime.today().strftime('%Y-%m-%d %H:%M'))
     common.Log('Starting DBUpdate')
     Notif = xbmcgui.Dialog().notification
-    Notif(common.__plugin__, common.getString(30106), sound = False)
+    Notif(common.__plugin__, common.getString(30106), sound=False)
     tv.addTVdb(False)
     movies.addMoviesdb(False)
     NewAsins = common.getCategories()
@@ -294,5 +320,5 @@ def updateAll():
     tv.updateFanart()
     common.addon.setSetting('last_update', datetime.today().strftime('%Y-%m-%d'))
     common.addon.setSetting('update_running', 'false')
-    Notif(common.__plugin__, common.getString(30126), sound = False)
+    Notif(common.__plugin__, common.getString(30126), sound=False)
     common.Log('DBUpdate finished')
